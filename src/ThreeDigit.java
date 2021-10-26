f880c7dd253ca9170636fc2e2bbe5db62ff23fcc
MomoDaBro <momofqais@gmail.com>
2021-10-26 13:24:34 -0500

    laptop changes

---
+++ src/ThreeDigit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ThreeDigit {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a three digit number");
+        String number = scanner.nextLine();
+        int digits = Integer.parseInt(number);
+
+        int third = digits / 100;
+        System.out.println("There is a " + third + " in the third digit place");
+
+        int second =  (digits-(third*100)) / 10;
+        System.out.println("There is a " + second + " in the second digit place");
+
+        int first = (digits-(third*100)-(second*10));
+        System.out.println("There is a " + first + " in the first digit place");
+    }
+}
