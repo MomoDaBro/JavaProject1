20dfed1ff2c29669038631a3599b209d701eb253
MomoDaBro <Momofqais@gmail.com>
2021-11-14 21:49:40 -0600

    desktop changes

---
+++ src/ArrayPractice.java
@@ -0,0 +1,22 @@
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3};
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        for (int e: arr) {
+            System.out.println(e);
+
+        }
+
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr.length);
+    }
+
+}
+
