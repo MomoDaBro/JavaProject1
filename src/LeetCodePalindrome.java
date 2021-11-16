c439afb46d4f3c5628bf98acb08eff23f65b1c99
MomoDaBro <Momofqais@gmail.com>
2021-11-15 21:59:54 -0600

    desktop changes

--- src/LeetCodePalindrome.java
+++ src/LeetCodePalindrome.java
@@ -1,24 +1,15 @@
 public class LeetCodePalindrome {
 
-    public static void isPalindrome(int x) {
+    public boolean isPalindrome(int x) {
 
-        x = 121;
+        return false;
+    }}
 
-        int third = x / 100;
-        System.out.println("There is a " + third + " in the third digit place");
-
-        int second =  (x-(third*100)) / 10;
-        System.out.println("There is a " + second + " in the second digit place");
-
-        int first = (x-(third*100)-(second*10));
-        System.out.println("There is a " + first + " in the first digit place");
-
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
-
-//        int reverse =
-//        if(x = reverse)
-//            System.out.println("true");
-    }
-}
+//    public static void main(String[] args) {
+//
+////        int x = 121;
+////        String ExpectedOutput = "true";
+////        String ActualOutput =
+////    }
+////}
+////
