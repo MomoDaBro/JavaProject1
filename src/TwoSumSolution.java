6d2974175d45c69cbd272cc122a956245e9302d1
MomoDaBro <momofqais@gmail.com>
2021-11-17 11:42:36 -0600

    WOOO I FIGURED IT OUT

--- src/TwoSumSolution.java
+++ src/TwoSumSolution.java
@@ -3,20 +3,26 @@ import java.util.Arrays;
 public class TwoSumSolution {
 
     public static int[] twoSum(int[] nums, int target) {
-        System.out.println(nums.length);
-        return nums;
+        int i;
+        for (i = 0; i + 1 < nums.length; i++)
+            if (nums[i] + nums[i + 1] == target) {
+                System.out.println("true");
+                break;
+            }
+        return new int[]{i, i + 1};
     }
 
     public static void main(String[] args) {
-        int[] nums= {2,7,11,15};
-        int target = 9;
+        int[] nums= {3,3};
+        int target = 6;
         int[] ExpectedOutput ={0, 1};
         int[] ActualOutput = twoSum(nums,target);
         System.out.println(Arrays.toString(ActualOutput));
+    }
 
 
     }
-}
+
 
 
 
