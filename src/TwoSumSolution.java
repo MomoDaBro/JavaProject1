d5301e0200ab431cfba0cbf75bb4863b25126044
MomoDaBro <momofqais@gmail.com>
2021-11-17 14:20:41 -0600

    nvm i didnt figure it out..

--- src/TwoSumSolution.java
+++ src/TwoSumSolution.java
@@ -4,25 +4,26 @@ public class TwoSumSolution {
 
     public static int[] twoSum(int[] nums, int target) {
         int i;
+
         for (i = 0; i + 1 < nums.length; i++)
             if (nums[i] + nums[i + 1] == target) {
                 System.out.println("true");
-                break;
-            }
-        return new int[]{i, i + 1};
+                return new int[]{i, i + 1};
+                }
+
+        for (i = 0; i + 1 < nums.length; i++)
+            if (nums[i] + nums[i + 2] == target) {
+                System.out.println("true");
+                break;}
+                return new int[]{i, i + 2};
+
     }
 
     public static void main(String[] args) {
-        int[] nums= {3,3};
-        int target = 6;
-        int[] ExpectedOutput ={0, 1};
-        int[] ActualOutput = twoSum(nums,target);
+        int[] nums = {0,4,3,0};
+        int target = 0;
+        int[] ExpectedOutput = {0, 1};
+        int[] ActualOutput = twoSum(nums, target);
         System.out.println(Arrays.toString(ActualOutput));
     }
-
-
-    }
-
-
-
-
+}
