26169b5719aa2eff095761b65a287b59cfbec489
MomoDaBro <Momofqais@gmail.com>
2021-11-17 19:29:31 -0600

    LETS GO I DID TWO LEETCODE PROBLEMS IN 1 DAY

--- src/LeetCodePalindrome.java
+++ src/LeetCodePalindrome.java
@@ -1,15 +1,36 @@
 public class LeetCodePalindrome {
 
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
+        String y = Integer.toString(x);
+        if (x < 0)
+            return false;
+        String abc = "";
+        for (int i = y.length() - 1; i >= 0; i--) {
+            char a = y.charAt(i);
+            abc = abc + a;
+        }
+//        if (abc.equals(y) == true)
+//            return true;
 
-        return false;
-    }}
+        return abc.equals(y);
+    }
 
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
+        int x = 121;
+        isPalindrome(x);
+        String ExpectedOutput = "true";
+        System.out.println(isPalindrome(x));
+    }
+//      for (int j = 1; j < y.length(); j++)
+//            for (int i = 0; i +1 < y.length(); i++) {
+////  1. get each index for the first characrter of a string
+//                char a = y.charAt(i);
+////  2. add that string to string cba
+//                String cba = "" +a;
 //
-////        int x = 121;
-////        String ExpectedOutput = "true";
-////        String ActualOutput =
-////    }
-////}
-////
+//            int abc = Integer.parseInt(cba);
+////  4. Compare it to x
+//                if (abc == x)
+//                    return true;
+//            }
+}
--- src/TwoSumSolution.java
+++ src/TwoSumSolution.java
@@ -3,27 +3,31 @@ import java.util.Arrays;
 public class TwoSumSolution {
 
     public static int[] twoSum(int[] nums, int target) {
-        int i;
-
-        for (i = 0; i + 1 < nums.length; i++)
-            if (nums[i] + nums[i + 1] == target) {
-                System.out.println("true");
-                return new int[]{i, i + 1};
+        for (int j = 1; j < nums.length; j++)
+            for (int i = 0; i + j < nums.length; i++)
+                if (nums[i] + nums[i + j] == target) {
+                    System.out.println("true");
+                    return new int[]{i, i + j};
                 }
 
-        for (i = 0; i + 1 < nums.length; i++)
-            if (nums[i] + nums[i + 2] == target) {
-                System.out.println("true");
-                break;}
-                return new int[]{i, i + 2};
-
+        return new int[]{};
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,4,3,0};
-        int target = 0;
-        int[] ExpectedOutput = {0, 1};
+        int[] nums = {3, 0,0,0,0,0, 3};
+        int target = 6;
+        int[] ExpectedOutput = {0, 6};
         int[] ActualOutput = twoSum(nums, target);
         System.out.println(Arrays.toString(ActualOutput));
     }
+
+//    for (i = 0; i + 2 < nums.length; i++)
+//            if (nums[i] + nums[i + 2] == target) {
+//        System.out.println("true");
+//        return new int[]{i, i + 2};
+//    }
+//        for (i = 0; i + 3 < nums.length; i++)
+//            if (nums[i] + nums[i + 3] == target) {
+//        System.out.println("true");
+//        return new int[]{i, i + 3};
 }
