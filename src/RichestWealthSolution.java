1fedd0b224444f84071e6a4cf94a7fd4b9370760
MomoDaBro <momofqais@gmail.com>
2021-11-19 13:10:31 -0600

    RichestWealth solution attempt 1

---
+++ src/RichestWealthSolution.java
@@ -0,0 +1,22 @@
+public class RichestWealthSolution {
+    public int maximumWealth(int[][] accounts) {
+
+// 1. Find out the length of the integer grid and the amount of integers within each account
+
+        int i;
+        int m = accounts.length;
+        int n = accounts[i].length;
+
+        for(i = 0; i < m; i++)
+            for(int j = 0; j < n; i++)
+                int a
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[][] accounts :[1,2,3],[3,2,1]
+    }
+}
