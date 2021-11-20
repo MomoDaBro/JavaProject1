93df879ae6a86160d2777ad83c4257a1062407b4
MomoDaBro <Momofqais@gmail.com>
2021-11-19 22:39:46 -0600

    Finished another leetcode stupid thing im built diff

--- src/RichestWealthSolution.java
+++ src/RichestWealthSolution.java
@@ -2,21 +2,26 @@ public class RichestWealthSolution {
     public int maximumWealth(int[][] accounts) {
 
 // 1. Find out the length of the integer grid and the amount of integers within each account
+// 2. Add all of the integers in each array
+// 3. compare each final array integer
 
-        int i;
         int m = accounts.length;
-        int n = accounts[i].length;
+        int n = accounts[0].length;
 
-        for(i = 0; i < m; i++)
-            for(int j = 0; j < n; i++)
-                int a
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int a = accounts[i][j];
+                int b = +a;
+
+            }
 
 
-
-
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
-        int[][] accounts :[1,2,3],[3,2,1]
+        int[][] accounts = {{1, 2, 3}, {3, 2, 1}};
     }
+
 }
