7f0e5a2596755b12643c2733764ebd5989e237d6
MomoDaBro <Momofqais@gmail.com>
2021-12-03 16:16:25 -0600

    i fixed it

--- src/FizzbuttsButSum.java
+++ src/FizzbuttsButSum.java
@@ -3,7 +3,7 @@ public class FizzbuttsButSum {
     public static void main(String[] arg) {
 
         int b = 0;
-        for (int i = 1; i <= 101; i++) {
+        for (int i = 0; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 b = (i * 4) + b;
 
@@ -13,12 +13,14 @@ public class FizzbuttsButSum {
             } else if (i % 5 == 0) {
                 b = (i * 3) + b;
 
+            } else {
+                b = i + b;
             }
 
         }
         System.out.println(b);
     }
-// they rbn it in python bnd got 8833
+
 }
 //        for (int i = 0; i <= 100; i++) {
 //            int b = 0;
