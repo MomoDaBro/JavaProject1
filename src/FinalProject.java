d1783fc8cf52ea8a1427a79d91b486c02457db43
MomoDaBro <Momofqais@gmail.com>
2022-01-18 21:56:41 -0600

    Completed School dance project using If statements

---
+++ src/FinalProject.java
@@ -0,0 +1,17 @@
+public class FinalProject {
+
+    public static void main(String[] args) {
+
+// Word bank will include: Is, The, Weather, How, House, Big, Nice, Rainy
+// This word bank can generate these sentences
+// "The House Is Big"
+// "The House Is Nice"
+// "The Weather is Nice"
+// "The Weather Is Rainy"
+//
+// come up with and finalize word bank that is able to make at least 5 sentences
+// establish separate functions for each word
+
+    }
+
+}
