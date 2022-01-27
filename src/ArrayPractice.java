76bae8c19dee6c4cf370d0e98dba60a0f803a16a
momoqais <momofqais@gmail.com>
2022-01-26 23:36:11 -0600

    macbook commit test

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -12,10 +12,6 @@ public class ArrayPractice {
 
         }
 
-//        System.out.println(arr[0]);
-//        System.out.println(arr[1]);
-//        System.out.println(arr[2]);
-//        System.out.println(arr.length);
     }
 
 }
